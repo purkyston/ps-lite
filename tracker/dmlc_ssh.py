371f53d8bfc938657905edae650883d903368610
Peikai Zheng <bnuzpk@gmail.com>
2018-05-15 01:58:34 +0800

    add file copy

--- tracker/dmlc_ssh.py
+++ tracker/dmlc_ssh.py
@@ -27,6 +27,8 @@ class SSHLauncher(object):
             if len(h.strip()) > 0:
                 self.hosts.append(h.strip())
 
+        self.train_file = args.train_file
+
     def sync_dir(self, local_dir, slave_node, slave_dir):
         """
         sync the working directory from root node into slave node
@@ -39,6 +41,13 @@ class SSHLauncher(object):
             local_dir, remote)
         subprocess.check_call([prog], shell = True)
 
+    def sync_file(self, local_file, slave_node, slave_file):
+        remote = slave_node + ':' + slave_file
+        logging.info('rsync %s -> %s', local_file, remote)
+        prog = 'rsync -az --rsh="ssh -o StrictHostKeyChecking=no" %s %s' % (
+            local_file, remote
+        )
+        subprocess.check_call([prog], shell = True)
 
     def get_env(self, pass_envs):
         envs = []
@@ -70,11 +79,34 @@ class SSHLauncher(object):
                 for h in self.hosts:
                     self.sync_dir(local_dir, h, working_dir)
 
+            train_file_list = []
+            if self.args.train_file is not None:
+                local_file = self.args.train_file
+                with open(local_file, 'r') as fin:
+                    data = fin.readlines()
+                    print len(data)
+                    cur_row = 0
+                    num = int(len(data) * 1.0 / nworker)
+                    for i in range(nworker):
+                        filename = os.path.basename(local_file)
+                        sliced_file = '/tmp/' + filename + '_' + str(i)
+                        dst_file = sliced_file + '.copy'
+                        train_file_list.append((sliced_file,dst_file))
+                        with open(sliced_file, 'w') as fout:
+                            fout.writelines(data[cur_row: cur_row + num])
+                        cur_row += num
+
             # launch jobs
+            print len(train_file_list)
+            worker_count = 0
             for i in range(nworker + nserver):
                 pass_envs['DMLC_ROLE'] = 'server' if i < nserver else 'worker'
                 node = self.hosts[i % len(self.hosts)]
                 prog = self.get_env(pass_envs) + ' cd ' + working_dir + '; ' + self.cmd
+                if self.args.train_file is not None and pass_envs['DMLC_ROLE'] == 'worker':
+                    self.sync_file(train_file_list[worker_count][0], node, train_file_list[worker_count][1])
+                    prog += ' ' + train_file_list[worker_count][1]
+                    worker_count += 1
                 prog = 'ssh -o StrictHostKeyChecking=no ' + node + ' \'' + prog + '\''
 
                 thread = Thread(target = run, args=(prog,))
@@ -103,6 +135,9 @@ def main():
     parser.add_argument('--sync-dir', type=str,
                         help = 'if specificed, it will sync the current \
                         directory into slave machines\'s SYNC_DIR')
+    parser.add_argument('--train-file', type=str,
+                        help = 'must be specificed, \
+                               tracker will slice train data automatically')
 
     args, unknown = parser.parse_known_args()
 
